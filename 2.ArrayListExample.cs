f446f0fc90e628b3c6a3ff4aa06207746f4c893d
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-12-05 00:18:52 +0530

    Add project files.

---
+++ 2.ArrayListExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+
+namespace CsharpCourse_DataStructures
+{
+    class ArrayListExample
+    {
+        static void Main()
+        {
+            ArrayList data = new ArrayList();
+
+
+
+            //object ---- int , decimal , bool , string
+
+
+            //addthe numbers
+            data.Add(10);
+            data.Add(20);
+            data.Add(30);
+            data.Add(40);   // 10000
+            data.Remove(30);
+            data.Add("Apple");
+            data.Add(true);  // add
+            data[3] = 100;
+            foreach (dynamic i in data)
+            {
+                Console.WriteLine(i);
+            }
+                //Console.WriteLine(data);
+            
+
+            //int            = object;      type cast  object into int 
+            int secondValue = (int)data[1];   //step1 we converted   // GET
+            Console.WriteLine(secondValue);
+
+            data[3] = 10000;   // update
+            data.Remove(30);  // remove
+
+
+
+            Console.WriteLine(data);
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Dec_Step4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
